b7dc5be7e821067641543c4b7825b6f3ad2f0afe
cgartco6 <155203611+cgartco6@users.noreply.github.com>
2026-04-29 22:25:30 +0200

    Update emailService.js

--- services/emailService.js
+++ services/emailService.js
@@ -4,13 +4,16 @@ const nodemailer = require('nodemailer');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 async function sendOrderConfirmation(order, userEmail) {
-  const msg = {
-    to: userEmail,
-    from: process.env.EMAIL_FROM,
-    subject: `Order Confirmation #${order.orderId}`,
-    html: `<h1>Thank you for your purchase!</h1><p>Order total: R${order.totalAmount}</p>...`
-  };
+  const html = `
+    <h1>Thank you for your order, ${order.customer.name}!</h1>
+    <p>Order #${order.orderId}</p>
+    <p>Total: R${order.totalAmount}</p>
+    <p>We'll notify you when your posters ship.</p>
+  `;
+  const msg = { to: userEmail, from: process.env.EMAIL_FROM, subject: `Order Confirmation #${order.orderId}`, html };
   await sgMail.send(msg);
 }
+
+async function sendPasswordReset(email, token) { /* implement */ }
 
 module.exports = { sendOrderConfirmation };
